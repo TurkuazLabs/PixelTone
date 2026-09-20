65ce678affe5059f716edc44d5da99dd63b993fc
Fury <FuryTr@yandex.com>
2026-09-21 00:56:41 +0300

    feat: register desktop controller

--- src-tauri/src/controllers/mod.rs
+++ src-tauri/src/controllers/mod.rs
@@ -2,10 +2,11 @@
 // # 📌 Amac: Rust controller modullerini disa acmak
 // # 📌 Controller - Rust
 // # Version: 1.0.0
-// # Aciklama: Color, palette ve settings controller baglantilarini tanimlar
+// # Aciklama: Color, desktop, palette ve settings controller baglantilarini tanimlar
 //
 // Bagimli Oldugu Katman: Controller
 
 pub mod color_controller;
+pub mod desktop_controller;
 pub mod palette_controller;
 pub mod settings_controller;
