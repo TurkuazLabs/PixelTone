7c16c55f9f3670bca91d368439f44cadee0c5163
Fury <FuryTr@yandex.com>
2026-09-21 17:15:02 +0300

    fix: expose non-destructive shortcut registration

--- frontend/tools/shortcut_tool.js
+++ frontend/tools/shortcut_tool.js
@@ -2,7 +2,7 @@
 // # 📌 Amac: Tauri global shortcut pluginini frontend servislerinden soyutlamak
 // # 📌 Tool - JavaScript
 // # Version: 1.0.0
-// # Aciklama: Runtime picker kisayolunu kaydetme, degistirme ve kaldirma adaptorudur
+// # Aciklama: Runtime picker kisayolunu kaydetme, durum sorgulama ve kaldirma adaptorudur
 //
 // Bagimli Oldugu Katman: Tool
 
@@ -15,16 +15,16 @@ import {
 import { APP_CONFIG } from "../config/app_config.js";
 
 export const shortcutTool = Object.freeze({
-  async replace(shortcut, handler) {
-    if (await isRegistered(shortcut)) {
-      await unregister(shortcut);
-    }
-
+  async register(shortcut, handler) {
     await register(shortcut, (event) => {
       if (event.state === APP_CONFIG.picker.shortcutPressedState) {
         handler(event);
       }
     });
+  },
+
+  async isRegistered(shortcut) {
+    return isRegistered(shortcut);
   },
 
   async unregister(shortcut) {
