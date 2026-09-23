bce8525ce3b40beaf216d3fedf08e298e83f477a
Fury <FuryTr@yandex.com>
2026-09-23 09:38:02 +0300

    fix: localize sidebar without reload

--- frontend/controllers/navigation_controller.js
+++ frontend/controllers/navigation_controller.js
@@ -1,8 +1,8 @@
 // # 📄 Dosya Yolu: pixeltone/frontend/controllers/navigation_controller.js
 // # 📌 Amac: Sol sidebar kullanici olaylarini NavigationService katmanina aktarmak
 // # 📌 Controller - JavaScript
-// # Version: 1.2.0
-// # Aciklama: Tercihler yuklendikten sonra lokalize sidebar'i baslatir ve sayfa gecislerini koordine eder
+// # Version: 1.2.1
+// # Aciklama: Lokalize sidebar'i baslatir, sayfa gecislerini koordine eder ve dil degisimini reload olmadan uygular
 //
 // Bagimli Oldugu Katman: Controller
 
@@ -11,12 +11,24 @@ import { navigationService } from "../services/navigation_service.js";
 import { preferenceService } from "../services/preference_service.js";
 import { navigationView } from "../views/navigation_view.js";
 
+function renderLabels() {
+  navigationView.initialize(languageService.getLabels());
+}
+
 async function boot() {
   await preferenceService.initialize();
-  navigationView.initialize(languageService.getLabels());
+  renderLabels();
+
   navigationView.bindSelect((page) => {
     navigationView.render(navigationService.select(page));
   });
+
+  preferenceService.subscribe((change) => {
+    if (change.languageChanged) {
+      renderLabels();
+    }
+  });
+
   navigationView.render(navigationService.getCurrent());
 }
 
