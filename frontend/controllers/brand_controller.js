c669215fb6493fc38a67959ebc01d5126f878102
Fury <FuryTr@yandex.com>
2026-09-23 09:03:23 +0300

    feat: wait for preferences before brand render

--- frontend/controllers/brand_controller.js
+++ frontend/controllers/brand_controller.js
@@ -1,15 +1,17 @@
 // # 📄 Dosya Yolu: pixeltone/frontend/controllers/brand_controller.js
 // # 📌 Amac: Ana arayuz TurkuazLabs marka baslangic istegini BrandService katmanina aktarmak
 // # 📌 Controller - JavaScript
-// # Version: 1.1.1
+// # Version: 1.2.0
 // # Aciklama: Marka footer baslangicini is kurali uygulamadan Service katmanina yonlendirir
 //
 // Bagimli Oldugu Katman: Controller
 
 import { brandService } from "../services/brand_service.js";
+import { preferenceService } from "../services/preference_service.js";
 
-function boot() {
+async function boot() {
+  await preferenceService.initialize();
   brandService.initialize();
 }
 
-boot();
+void boot();
